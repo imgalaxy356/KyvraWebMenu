8e24db0f63414ba27124d970820f847c50fd85af
imgalaxy356 <imgalaxy356@outlook.com>
2025-05-18 18:00:57 -0500

    Initial commit of Lust DMA web menu

---
+++ MAKEFILE.txt
@@ -0,0 +1,9 @@
+CC = gcc
+CFLAGS = -Wall -O2
+LDFLAGS = -lpthread
+
+all:
+	$(CC) main.c civetweb.c -o webmenu $(CFLAGS) $(LDFLAGS)
+
+clean:
+	rm -f webmenu
---
+++ main.c
@@ -0,0 +1,44 @@
+#include "civetweb.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static int menu_handler(struct mg_connection *conn, void *ignored) {
+    mg_printf(conn,
+        "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
+        "<!DOCTYPE html><html><head><title>DMA Menu</title></head><body>"
+        "<h1>DMA Cheat Menu</h1>"
+        "<form>"
+        "Aimbot: <input type='checkbox' name='aimbot'><br><br>"
+        "Smoothing: <input type='range' min='1' max='100' value='25'><br><br>"
+        "ESP: <input type='checkbox' name='esp'><br><br>"
+        "<input type='submit' value='Apply'>"
+        "</form>"
+        "</body></html>");
+    return 200;
+}
+
+int main(void) {
+    const char *options[] = {
+        "listening_ports", "8080",
+        "num_threads", "4",
+        "document_root", ".",  // Optional: serve static files if needed
+        0
+    };
+
+    struct mg_callbacks callbacks;
+    memset(&callbacks, 0, sizeof(callbacks));
+
+    struct mg_context *ctx = mg_start(&callbacks, 0, options);
+    if (!ctx) {
+        printf("Failed to start CivetWeb server.\n");
+        return 1;
+    }
+
+    mg_set_request_handler(ctx, "/", menu_handler, 0);
+
+    printf("Server started on port 8080.\n");
+    while (1) sleep(1);
+    mg_stop(ctx);
+    return 0;
+}
